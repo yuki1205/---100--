65fd83a2f0b61a9a76f1a68f408c0ef7954b4469
yuki <yuki.majima.gm@gmail.com>
2026-09-04 23:43:55 +0900

    Refresh cached game interface

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'runaway-v2';
+const CACHE_NAME = 'runaway-v3';
 const APP_FILES = ['./', './index.html', './style.css', './app.js', './settings.js', './manifest.json', './assets/icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))));
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
